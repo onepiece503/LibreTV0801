86809c1f9b343d2985452751a5967527db85b26c
onepiece503 <1511125331@qq.com>
2025-12-29 15:31:56 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -137,16 +137,16 @@ const API_SITES = {
     //     name: 'jkun资源',
     //     adult: true
     // },
-    // bwzy: {
-    //     api: 'https://api.bwzym3u8.com',
-    //     name: '百万资源',
-    //     adult: true
-    // },
-    // souav: {
-    //     api: 'https://api.souavzy.vip',
-    //     name: 'souav资源',
-    //     adult: true
-    // },
+     bwzy: {
+         api: 'https://api.bwzym3u8.com',
+        name: '百万资源',
+      adult: true
+   },
+     souav: {
+         api: 'https://api.souavzy.vip',
+         name: 'souav资源',
+         adult: true
+     },
     // r155: {
     //     api: 'https://155api.com',
     //     name: '155资源',
